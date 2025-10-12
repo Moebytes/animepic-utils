d59036ea1c2fe6c7a5fa8e48b133485d6d1e74c4
Tenpi <pie4chan@gmail.com>
2025-10-12 19:25:43 -0400

    bugfix

--- pics.ts
+++ pics.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import imageUtils from "./image-utils"
 
 const start = async () => {
-    console.log(imageUtils.addQualifier("./downloads", "g"))
+    console.log(imageUtils.changeQualifiers("./downloads", "g"))
 }
 
 start()
